099538882a94ba4c7fd5e80cb20c51c5a979283f
jojo080889 <jojo080889@gmail.com>
2011-06-16 15:22:54 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Comicbutler::Application.config.secret_token = '08b0d737b997ef5727f8ee0d8446bb0aeb6edeaef3a3e3d2bd02f996dd637640f93731ca216f06e1a1b6790d1bae3abd769710a7950f6167581c3326042b63db'
